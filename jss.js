eb810830123cf8b6250a3b453be47220c496b2b8
luchlima <92007053+luchlima@users.noreply.github.com>
2023-06-15 18:52:14 -0400

    edit2

---
+++ jss.js
@@ -0,0 +1,31 @@
+const caixa_frutas[
+    {
+        id = 1,
+        nome = 'banana',
+        img = 
+    }
+    {
+        id = 2,
+        nome = 'laranja',
+        img = 
+    }
+    {
+        id = 3,
+        nome = 'goiaba',
+        img = 
+    }
+    {
+        id = 4,
+        nome = 'pera',
+        img = 
+    }
+    {
+        id = 5,
+        nome = 'uva',
+        img = 
+    }
+
+
+
+
+]
